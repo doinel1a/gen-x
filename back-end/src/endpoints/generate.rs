26cb4f986e57fc6d928603d1db6ea298d9ff4b72
doinel1a <doinel1atanasiu@gmail.com>
2023-10-12 11:57:01 +0200

    refactor(generate-ep): code splitted up for readability and maintainability

--- back-end/src/endpoints/generate.rs
+++ back-end/src/endpoints/generate.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io::{Cursor, Read, Write};
 
 use actix_web::web::{Bytes, Json};
@@ -29,60 +30,18 @@ async fn generate(body: Json<Body>) -> Result<HttpResponse, Error> {
     let sc_abi = &body.sc_abi;
     let sc_name = body.sc_abi.name();
     let endpoints = body.sc_abi.endpoints();
+    let mut endpoints_props = get_readonly_endpoints_props(endpoints);
 
-    add_dapp_bootstrapper_files(
+    let _ = add_dapp_bootstrapper_files(
         Path::new(&DAPP_BOOTSTRAPPER_URI),
         Path::new(&DAPP_BOOTSTRAPPER_URI),
         &mut zip_writer,
         zip_options,
     )?;
 
-    add_sc_abi_file(&sc_abi, &mut zip_writer, zip_options)?;
+    let _ = add_sc_abi_file(&sc_abi, &mut zip_writer, zip_options)?;
 
-    let mut endpoints_props = get_readonly_endpoints_props(endpoints);
-    let does_page_contain_dashboard = endpoints_props
-        .keys()
-        .any(|(_, page_name)| page_name == "dashboard");
-
-    if !does_page_contain_dashboard {
-        endpoints_props.insert(
-            ("/pages".to_string(), "dashboard".to_string()),
-            Vec::<ReadonlyEndpointProps>::new(),
-        );
-    }
-
-    let routes: Vec<(String, String)> = endpoints_props
-        .keys()
-        .filter(|(folder, page_name)| !folder.is_empty() && !page_name.is_empty())
-        .cloned()
-        .collect();
-
-    add_dynamic_pages_to_routes(&routes, &mut zip_writer, zip_options)?;
-
-    for ((folder, page_name), ep_props) in endpoints_props {
-        for props in &ep_props {
-            let rendered_endpoint = readonly_endpoint::render(
-                &props.hook_name,
-                &props.name,
-                &props.inputs,
-                &props.outputs,
-            );
-
-            zip_writer
-                .start_file(format!("src/hooks/{}.ts", &props.file_name), zip_options)
-                .unwrap();
-            zip_writer.write_all(rendered_endpoint.as_bytes()).unwrap();
-        }
-
-        if !folder.is_empty() && !page_name.is_empty() {
-            let rendered_page = page::render(&page_name, &ep_props);
-
-            zip_writer
-                .start_file(format!("src{}/{}.tsx", folder, page_name), zip_options)
-                .unwrap();
-            zip_writer.write_all(rendered_page.as_bytes()).unwrap();
-        }
-    }
+    let _ = add_readonly_endpoints_files(&mut endpoints_props, &mut zip_writer, zip_options)?;
 
     let end_cursor = zip_writer.finish().unwrap();
     let zip_buffer = end_cursor.into_inner();
@@ -139,6 +98,60 @@ fn add_sc_abi_file(
         .start_file("src/contracts-abi/contract.abi.json", zip_options)
         .unwrap();
     zip_writer.write_all(&abi_file.as_bytes()).unwrap();
+
+    Ok(())
+}
+
+fn add_readonly_endpoints_files(
+    endpoints_props: &mut HashMap<(String, String), Vec<ReadonlyEndpointProps>>,
+    zip_writer: &mut ZipWriter<Cursor<&mut Vec<u8>>>,
+    zip_options: FileOptions,
+) -> Result<(), std::io::Error> {
+    let does_page_contain_dashboard = endpoints_props
+        .keys()
+        .any(|(_, page_name)| page_name == "dashboard");
+
+    println!("DASH {}", does_page_contain_dashboard);
+
+    if !does_page_contain_dashboard {
+        endpoints_props.insert(
+            ("/pages".to_string(), "dashboard".to_string()),
+            Vec::<ReadonlyEndpointProps>::new(),
+        );
+    }
+
+    for ((folder, page_name), ep_props) in &mut *endpoints_props {
+        for props in &mut *ep_props {
+            let rendered_endpoint = readonly_endpoint::render(
+                &props.hook_name,
+                &props.name,
+                &props.inputs,
+                &props.outputs,
+            );
+
+            zip_writer
+                .start_file(format!("src/hooks/{}.ts", &props.file_name), zip_options)
+                .unwrap();
+            zip_writer.write_all(rendered_endpoint.as_bytes()).unwrap();
+        }
+
+        if !folder.is_empty() && !page_name.is_empty() {
+            let rendered_page = page::render(&page_name, &ep_props);
+
+            zip_writer
+                .start_file(format!("src{}/{}.tsx", folder, page_name), zip_options)
+                .unwrap();
+            zip_writer.write_all(rendered_page.as_bytes()).unwrap();
+        }
+    }
+
+    let routes: Vec<(String, String)> = endpoints_props
+        .keys()
+        .filter(|(folder, page_name)| !folder.is_empty() && !page_name.is_empty())
+        .cloned()
+        .collect();
+
+    add_dynamic_pages_to_routes(&routes, zip_writer, zip_options)?;
 
     Ok(())
 }
